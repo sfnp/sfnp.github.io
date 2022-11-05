26cfecc357328f738cd13ab8d22f76146de72f92
SOFANdev <sofanpaudel222@gmail.com>
2022-11-05 13:51:57 +0545

    694

--- trailer.js
+++ trailer.js
@@ -1,37 +1,71 @@
 const trailer = document.getElementById("trailer");
 
-const animateTrailer = (e, interacting) => {
-  const x = e.clientX - trailer.offsetWidth / 2,
-        y = e.clientY - trailer.offsetHeight / 2;
-  // const x = e.clientX - trailer.offsetWidth,
-  // y = e.clientY - trailer.offsetHeight;
-  
-  const keyframes = {
-    transform: `translate(${x}px, ${y}px) scale(${interacting ? 2 : 1})`,
-    border: `1px solid ${interacting ? '#2f3136' : '#fff'}`,
-  }
-  
-  trailer.animate(keyframes, { 
-    duration: 800, 
-    fill: "forwards" 
-  });
+function rainbow() {
+  trailer.style.backgroundColor = "red";
 }
 
-const getTrailerClass = type => {
-      return "fa-solid fa-arrow-up-right"; 
+function polako() {
+  trailer.style.backgroundColor = "white";
+}
+
+const animateTrailer = (e, interacting) => {
+  const x = e.clientX - trailer.offsetWidth / 2,
+    y = e.clientY - trailer.offsetHeight / 2;
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 2 : 1})`,
+    border: `1px solid ${interacting ? 'white' : 'transparent'}`,
   }
 
+
+  trailer.animate(keyframes, {
+    duration: 800,
+    fill: "forwards"
+  });
+
+  // trailer.animate(bruh, {
+  //   duration: 500,
+  //   fill: 'backwards',
+  // })
+}
+
+
+const getTrailerClass = type => {
+  return "fa-solid fa-arrow-up";
+}
+
+const getTrailerText = type => {
+  return 'bruh';
+}
+
 window.onmousemove = e => {
-  const interactable = e.target.closest(".interactable"),
-        interacting = interactable !== null;
-  
+  const interactable = e.target.closest(".tra")
+  const text = e.target.closest(".text"),
+    interacting = interactable !== null;
+  textable = text !== null;
+
   const icon = document.getElementById("trailer-icon");
-  
-  animateTrailer(e, interacting);
-  
+
+  animateTrailer(e, interacting, text);
+
   trailer.dataset.type = interacting ? interactable.dataset.type : "";
-  
-  if(interacting) {
+
+  if (interacting) {
     icon.className = getTrailerClass(interactable.dataset.type);
   }
 }
+
+document.addEventListener('keydown', r => {
+    if (r.keyCode === 82) {
+      if (trailer.style.backgroundColor !== 'red') {
+        rainbow()
+        alert('You have unlocked a rainbow color for your cursor. press d to remove it.');
+      }
+    }
+
+    if (r.keyCode === 68) {
+      if (trailer.style.backgroundColor === 'red') {
+      alert('Removed rainbow color.');
+      polako();
+      }
+    }
+})
